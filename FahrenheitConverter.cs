4600ed659afd87376708e09a38469adcc2e516aa
Tec <Tec@SKAB1-PC-03>
2021-08-11 12:49:28 +0200

    Add project files.

---
+++ FahrenheitConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1MethodTry
+{
+    class FahrenheitConverter
+    {
+        //Method input
+        public void InputMethod()
+        {
+            int result = 0;
+            bool canItConvert = false;
+            do
+            {
+                Console.WriteLine("*** Fahrenheit omregner ***");
+                Console.WriteLine("Input Fahrenheit:");
+                string str = Console.ReadLine();
+                canItConvert = int.TryParse(str, out result);
+            }
+            while (!canItConvert);
+
+            double c = ConversionMethod(result);
+            OutputMethod(c);
+        }
+
+
+
+
+
+
+        double ConversionMethod(int fahrenheit) //parameter krav bliver sat
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            double celsius2dec = Math.Round(celsius, 2);
+
+            //if (celsius2dec < 0) return celsius2dec;
+            //else return celsius
+            return celsius;
+        }
+        void OutputMethod(double cel)
+        {
+            Console.WriteLine("F to C " + cel);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace H1MethodTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input i F
+            //omregning til C
+            //udskrevet i C m. 2 dec.
+            //Again
+            //Console.WriteLine("Intast en temperatur i Farenheit i decimal tal for at omregkunne ");
+
+            FahrenheitConverter fahrenheitConverter= new FahrenheitConverter();
+            fahrenheitConverter.InputMethod();
+
+        }
+    }
+}
